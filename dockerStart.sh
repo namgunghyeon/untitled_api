0d5443746338b18bd3c65e8303bd4d6ac8da9716
남궁현 <nkh@encoredtech.com>
2017-08-28 00:44:45 +0900

    added container name in docker

--- dockerStart.sh
+++ dockerStart.sh
@@ -1,2 +1,2 @@
-docker stop $(docker ps -q --filter ancestor=untitled_api )
-docker run -d -p 8081:8081 --name untitled_api untitled_api
+docker stop $(docker ps -q --filter ancestor=run_untitled_api )
+docker run -d -p 8081:8081 --name run_untitled_api untitled_api
